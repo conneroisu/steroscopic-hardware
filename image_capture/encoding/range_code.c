323e30d1ffe947ce05bc47db4dc24b4b85209246
NolanEastburn <nolieman1@gmail.com>
2025-05-07 12:02:15 -0500

    -Wrote function to calculate the next range value.

--- image_capture/encoding/range_code.c
+++ image_capture/encoding/range_code.c
@@ -6,7 +6,25 @@ typedef struct
     size_t previous_count_sum;
 } counts_t;
 
+typedef struct
+{
+    uint32_t low;
+    uint32_t high;
+} range_t;
+
 const uint32_t MAX_RANGE = UINT32_MAX;
+
+range_t get_next_range(uint8_t symbol, range_t current_range, counts_t** counts, size_t size)
+{
+    range_t result = {.low = 0, .high = 0};
+
+    size_t prev_range_size = current_range.high - current_range.low;
+
+    result.low = (uint32_t) (((*counts)[symbol].previous_count_sum * prev_range_size) / size);
+    result.high = ((uint32_t) (((*counts)[symbol].current_count * prev_range_size) / size)) + result.low;
+
+    return result;
+}
 
 /* 
     Basic Description:
@@ -36,8 +54,7 @@ size_t range_code(uint8_t* uncoded, uint8_t* coded, size_t size)
     size_t result = 0;
     uint8_t* next_coded = coded;
 
-    uint32_t low = 0;
-    uint32_t high = MAX_RANGE;
+    range_t range = {.low = 0, .high = MAX_RANGE};
 
     // Store byte counts
     // This will be used to calculate probabilities without storing them as floating point.
@@ -57,7 +74,6 @@ size_t range_code(uint8_t* uncoded, uint8_t* coded, size_t size)
     {
         byte_counts[i].previous_count_sum = byte_counts[i - 1].previous_count_sum + byte_counts[i - 1].current_count;
     }
-
 
     // Calculate the number of MAX_RANGE blocks that we need to process.
     size_t block_count = size / 4;
